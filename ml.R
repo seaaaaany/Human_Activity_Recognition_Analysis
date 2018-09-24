7237d3349fac42bdb68d979e389716f1065f92df
Sean <yycyjqc@gmail.com>
2018-09-24 16:07:07 -0700

    clean the data, get new data set

---
+++ ml.R
@@ -0,0 +1,24 @@
+library(caret)
+library(randomForest)
+library(rpart)
+library(rpart.plot)
+
+# Loading the training data set
+# Replace all missing data with "NA"
+trainingset <- read.csv("pml-training.csv", na.strings=c("NA","#DIV/0!", ""))
+# Loading the testing data set 
+testingset <- read.csv('pml-testing.csv', na.strings=c("NA","#DIV/0!", ""))
+
+# Delete columns with missing values
+trainingset<-trainingset[,colSums(is.na(trainingset)) == 0]
+testingset <-testingset[,colSums(is.na(testingset)) == 0]
+
+# Delete irrelevant variables.
+trainingset   <-trainingset[,-c(1:7)]
+testingset <-testingset[,-c(1:7)]
+
+# Crate new data set
+dim(trainingset)
+dim(testingset)
+head(trainingset)
+head(testingset)
